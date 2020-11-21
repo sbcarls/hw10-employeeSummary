6959ea015efd1e95535937a66955d4fa60a41374
Sarah Carlson <sbcarls@gmail.com>
2020-11-21 17:57:36 -0500

    ran tests and made edits

--- test/manager.test.js
+++ test/manager.test.js
@@ -7,14 +7,14 @@ test("Can set office number via constructor argument", () => {
   expect(e.officeNumber).toBe(testValue);
 });
 
-test('getRole() should return "Manager"', () => {
+test('getPosition() should return "Manager"', () => {
   const testValue = "Manager";
   const e = new Manager("Foo", 1, "test@test.com", 100);
-  expect(e.getRole()).toBe(testValue);
+  expect(e.getPosition()).toBe(testValue);
 });
 
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
+  expect(e.officeNumber()).toBe(testValue);
 });
